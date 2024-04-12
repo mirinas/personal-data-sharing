72de74fdc08be26430c995bf1cd04735ee4944b8
mirinas <augustas.mirinas@gmail.com>
2024-04-12 22:50:20 +0300

    changed Org4 name, upgraded cc to 1.1

--- fablo-target/fabric-docker/channel-query-scripts.sh
+++ fablo-target/fabric-docker/channel-query-scripts.sh
@@ -27,7 +27,7 @@ channelQuery() {
     peerChannelListTls "cli.org3.gov" "peer0.org3.gov:7081" "crypto-orderer/tlsca.orderer.org-cert.pem"
 
   elif
-    [ "$1" = "list" ] && [ "$2" = "org4" ] && [ "$3" = "peer0" ]
+    [ "$1" = "list" ] && [ "$2" = "owners" ] && [ "$3" = "peer0" ]
   then
 
     peerChannelListTls "cli.owners.org" "peer0.owners.org:7101" "crypto-orderer/tlsca.orderer.org-cert.pem"
@@ -85,17 +85,17 @@ channelQuery() {
     peerChannelFetchBlockTls "ch1" "cli.org3.gov" "${BLOCK_NAME}" "peer0.org3.gov:7081" "crypto-orderer/tlsca.orderer.org-cert.pem" "$TARGET_FILE"
 
   elif
-    [ "$1" = "getinfo" ] && [ "$2" = "ch1" ] && [ "$3" = "org4" ] && [ "$4" = "peer0" ]
+    [ "$1" = "getinfo" ] && [ "$2" = "ch1" ] && [ "$3" = "owners" ] && [ "$4" = "peer0" ]
   then
 
     peerChannelGetInfoTls "ch1" "cli.owners.org" "peer0.owners.org:7101" "crypto-orderer/tlsca.orderer.org-cert.pem"
 
-  elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "ch1" ] && [ "$4" = "org4" ] && [ "$5" = "peer0" ]; then
+  elif [ "$1" = "fetch" ] && [ "$2" = "config" ] && [ "$3" = "ch1" ] && [ "$4" = "owners" ] && [ "$5" = "peer0" ]; then
     TARGET_FILE=${6:-"$channel-config.json"}
 
     peerChannelFetchConfigTls "ch1" "cli.owners.org" "$TARGET_FILE" "peer0.owners.org:7101" "crypto-orderer/tlsca.orderer.org-cert.pem"
 
-  elif [ "$1" = "fetch" ] && [ "$3" = "ch1" ] && [ "$4" = "org4" ] && [ "$5" = "peer0" ]; then
+  elif [ "$1" = "fetch" ] && [ "$3" = "ch1" ] && [ "$4" = "owners" ] && [ "$5" = "peer0" ]; then
     BLOCK_NAME=$2
     TARGET_FILE=${6:-"$BLOCK_NAME.block"}
 
@@ -126,8 +126,8 @@ printChannelsHelp() {
   echo -e "\t List channels on 'peer0' of 'Org3'".
   echo ""
 
-  echo "fablo channel list org4 peer0"
-  echo -e "\t List channels on 'peer0' of 'Org4'".
+  echo "fablo channel list owners peer0"
+  echo -e "\t List channels on 'peer0' of 'Owners'".
   echo ""
 
   echo "fablo channel getinfo ch1 org1 peer0"
@@ -160,14 +160,14 @@ printChannelsHelp() {
   echo -e "\t Fetch a block with given number and save it. Uses first peer 'peer0' of 'Org3'".
   echo ""
 
-  echo "fablo channel getinfo ch1 org4 peer0"
-  echo -e "\t Get channel info on 'peer0' of 'Org4'".
+  echo "fablo channel getinfo ch1 owners peer0"
+  echo -e "\t Get channel info on 'peer0' of 'Owners'".
   echo ""
-  echo "fablo channel fetch config ch1 org4 peer0 [file-name.json]"
-  echo -e "\t Download latest config block and save it. Uses first peer 'peer0' of 'Org4'".
+  echo "fablo channel fetch config ch1 owners peer0 [file-name.json]"
+  echo -e "\t Download latest config block and save it. Uses first peer 'peer0' of 'Owners'".
   echo ""
-  echo "fablo channel fetch <newest|oldest|block-number> ch1 org4 peer0 [file name]"
-  echo -e "\t Fetch a block with given number and save it. Uses first peer 'peer0' of 'Org4'".
+  echo "fablo channel fetch <newest|oldest|block-number> ch1 owners peer0 [file name]"
+  echo -e "\t Fetch a block with given number and save it. Uses first peer 'peer0' of 'Owners'".
   echo ""
 
 }
